3e9119c8fbcd897d7801fc73d4927dc70c66febf
Felipe Sousa <54862789+FelipeJSousa@users.noreply.github.com>
2021-10-24 14:52:28 -0300

    comit

---
+++ ApiClient/ProdutoClient.cs
@@ -0,0 +1,28 @@
+﻿using ApiClient.Interfaces;
+using Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    public class ProdutoClient : BaseClient<ProdutoModel>, IProdutoClient
+    {
+
+        public ProdutoClient()
+        {
+        }
+
+        public async Task<ReturnModel<List<ProdutoModel>>> GetAll(long usuario)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture, typeof(ProdutoModel).Name.Replace("Model", "")), $"idUsuario={usuario}");
+            return await GetAsync<List<ProdutoModel>>(requestUrl);
+        }
+
+        public async Task<ReturnModel<List<ProdutoModel>>> Get(long id, long usuario)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture, typeof(ProdutoModel).Name.Replace("Model", "")), $"id={id}&idUsuario={usuario}");
+            return await GetAsync<List<ProdutoModel>>(requestUrl);
+        }
+
+    }
+}
--- SellFlowWeb/Controllers/ProdutoController.cs
+++ SellFlowWeb/Controllers/ProdutoController.cs
@@ -4,9 +4,76 @@ namespace SellFlowWeb.Controllers
 {
     public class ProdutoController : Controller
     {
-        public IActionResult Index()
+        private readonly IProdutoClient _produtoClient;
+
+        public ProdutoController(IServiceProvider serviceProvider) : base(serviceProvider)
         {
-            return View();
+            _produtoClient = serviceProvider.GetService<IProdutoClient>();
         }
+
+        public async Task<IActionResult> Index(int IdUsuario)
+        {
+            var _ret = await _produtoClient.GetAll(IdUsuario);
+            var _produtos = new Mapper(AutoMapperConfig.RegisterMappings()).Map<IEnumerable<ProdutoDataView>>(_ret.dados);
+            return VerificarLogin(View(_produtos));
+        }
+
+        public IActionResult Criar()
+        {
+            @ViewBag.message = TempData["message"];
+            return VerificarLogin(View());
+        }
+
+        [Route("Editar/{usuario}/{id}")]
+        public async Task<IActionResult> Editar(long usuario, long id)
+        {
+            var _ret = await _produtoClient.Get(id);
+            @ViewBag.message = TempData["message"];
+            var _ret = await _produtoClient.Get(id, usuario);
+            var _produtos = new Mapper(AutoMapperConfig.RegisterMappings()).Map<IEnumerable<ProdutoDataView>>(_ret.dados);
+            return VerificarLogin(View(_produtos.FirstOrDefault()));
+        }
+
+        public async Task<IActionResult> Salvar(ProdutoApiRequest obj)
+        {
+            var redirectHome = VerificarLogin();
+            if (redirectHome is not null)
+            {
+                return redirectHome;
+            }
+
+            ReturnModel<ProdutoModel> _ret = new();
+
+            _ret = await _produtoClient.Save(obj);
+
+            if (!_ret.status)
+            {
+                TempData["message"] = "Não foi possível Salvar!";
+                return View("Editar",obj.id);
+            }
+
+            return View("Index");
+
+        }
+
+        public async Task<IActionResult> ExcluirAsync(long id)
+        {
+            var redirectHome = VerificarLogin();
+            if (redirectHome is not null)
+            {
+                return redirectHome;
+            }
+
+            ReturnModel<ProdutoModel> _ret = await _produtoClient.Delete(id);
+
+            if(!_ret.status)
+            {
+                TempData["message"] = "Não foi possível Excluir!";
+                return View("Index");
+            }
+
+            return View("Index");
+        }
+
     }
 }
