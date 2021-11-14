8d4556fbae2a98a5a6a98a730c67158bd2bca433
Felipe Sousa <54862789+FelipeJSousa@users.noreply.github.com>
2021-11-13 21:23:52 -0300

    Prod api

--- Models/BaseUrl.cs
+++ Models/BaseUrl.cs
@@ -2,7 +2,7 @@
 {
     public static class BaseUrl
     {
-        public static string Api = "http://localhost:5001/api/";
-        //public static string Api = "http://felipejsousa-001-site1.itempurl.com/api/";
+        //public static string Api = "http://localhost:5001/api/";
+        public static string Api = "http://felipejsousa-001-site1.itempurl.com/api/";
     }
 }
